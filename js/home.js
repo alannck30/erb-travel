9d698878b67b3d0a53f5342368ffdf0473e7882e
AlanNCK30 <alanngck30@gmail.com>
2026-03-29 22:12:14 +0800

    added auto carousel with pause

--- js/home.js
+++ js/home.js
@@ -5,8 +5,6 @@ const mobileMenu = document.querySelector(".navbar__mobile-menu-items");
 // Toggle menu on hamburger click
 toggleButton.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
-  console.log("click");
-  console.log(mobileMenu);
 });
 
 // Close menu when clicking a menu link
@@ -23,26 +21,69 @@ const date = (document.getElementById("date").innerHTML = new Date().getFullYear
 // #endregion
 
 // #region change background and video
+// the order of the images,videos and the cards in html must be the same
 const cards = document.querySelectorAll(".section__card-container");
 const bgImgs = document.querySelectorAll(".bg");
 const videos = document.querySelectorAll(".hero__video");
+const gridContainer = document.querySelector(".grid-container");
+const progressBars = document.querySelectorAll(".section__card-progress-fill");
 
-cards.forEach((card) => {
-  card.addEventListener("mouseenter", () => {
-    console.log("hovered");
-    bgImgs.forEach((bgImg) => {
-      bgImg.classList.remove("active");
-      if (card.classList.contains(bgImg.id)) {
-        bgImg.classList.add("active");
-      }
-    });
-    videos.forEach((video) => {
-      if (card.classList.contains(video.id.split("-")[1])) {
-        video.classList.add("active");
-      } else {
-        video.classList.remove("active");
-      }
+let realIndex = 0;
+
+function initCarousel() {
+  cards[0].classList.add("active");
+
+  progressBars.forEach((bar) => {
+    bar.addEventListener("animationend", () => {
+      const nextIndex = (realIndex + 1) % cards.length;
+      playNext(realIndex, nextIndex);
+      realIndex = nextIndex;
     });
   });
+}
+
+function playNext(prev, next) {
+  [cards, bgImgs, videos].forEach((list) => {
+    list[prev].classList.remove("active");
+    list[next].classList.add("active");
+  });
+}
+
+function updateDisplay(from, to) {
+  [bgImgs, videos].forEach((list) => {
+    list[from].classList.remove("active");
+    list[to].classList.add("active");
+  });
+}
+
+gridContainer.addEventListener("mouseover", (e) => {
+  const hoveredCard = e.target.closest(".section__card-container");
+
+  if (!hoveredCard) return;
+  progressBars.forEach((bar) => {
+    bar.style.animationPlayState = "paused";
+  });
+
+  const hoverIndex = [...cards].indexOf(hoveredCard);
+  if (hoverIndex !== realIndex) {
+    updateDisplay(realIndex, hoverIndex);
+  }
 });
+
+gridContainer.addEventListener("mouseout", (e) => {
+  const hoveredCard = e.target.closest(".section__card-container");
+
+  if (!hoveredCard) return;
+  progressBars.forEach((bar) => {
+    bar.style.animationPlayState = "running";
+  });
+
+  const visibleIndex = [...bgImgs].findIndex((img) => img.classList.contains("active"));
+  if (visibleIndex !== realIndex) {
+    updateDisplay(visibleIndex, realIndex);
+  }
+});
+
+initCarousel();
+
 // #endregion
